cd660f9d2de7dabc3bf7b182152df8a47b863579
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-05-11 20:16:28 +0530

    did something

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 15;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
