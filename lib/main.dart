9a1f6180e2c204cd38efc064aae76046efeed5a3
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-05-12 22:36:50 +0530

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/home_page.dart';
+import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:flutter_tutorial/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home: HomePage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/":(context) => HomePage(),
+        "/login":(context) => LoginPage(),
+      },
     );
   }
 }
