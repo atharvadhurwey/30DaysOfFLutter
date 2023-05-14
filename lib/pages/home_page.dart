ec95809858e70cc5087d3ab8a7bbda2347dc02e5
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-05-14 14:36:24 +0530

    did bunch of new stuffs, still idk what

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,206 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/models/catalog.dart';
 import 'package:flutter_tutorial/widgets/items_widget.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_tutorial/widgets/themes.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Codepur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catelog App"),
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
+        child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (CatalogModel.items != null &&
+                    CatalogModel.items!.isNotEmpty)
+                  CatalogList().expand()
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
+            )),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index]);
-          },
-        ),
-      ),
-      drawer: MyDrawer(),
     );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Catalog App"),
+  //     ),
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(16.0),
+  //       child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+  //           ? GridView.builder(
+  //               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+  //                   crossAxisCount: 2,
+  //                   mainAxisSpacing: 16,
+  //                   crossAxisSpacing: 16),
+  //               itemBuilder: (context, index) {
+  //                 final item = CatalogModel.items![index];
+  //                 return Card(
+  //                     clipBehavior: Clip.antiAlias,
+  //                     shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(10)),
+  //                     child: GridTile(
+  //                         header: Container(
+  //                             padding: const EdgeInsets.all(12),
+  //                             decoration: BoxDecoration(
+  //                               color: Colors.lightBlue,
+  //                             ),
+  //                             child: Text(
+  //                               item.name,
+  //                               style: TextStyle(color: Colors.white),
+  //                             )),
+  //                         footer: Container(
+  //                             padding: const EdgeInsets.all(12),
+  //                             decoration: BoxDecoration(
+  //                               color: Colors.black,
+  //                             ),
+  //                             child: Text(
+  //                               item.price.toString(),
+  //                               style: TextStyle(color: Colors.white),
+  //                             )),
+  //                         child: Image.network(item.image)));
+  //               },
+  //               itemCount: CatalogModel.items!.length,
+  //             )
+  //           // ? ListView.builder(
+  //           //     itemCount: CatalogModel.items!.length,
+  //           //     itemBuilder: (context, index) => ItemWidget(
+  //           //       item: CatalogModel.items![index],
+  //           //     ),
+  //           //   )
+  //           : Center(
+  //               child: CircularProgressIndicator(),
+  //             ),
+  //     ),
+  //     drawer: MyDrawer(),
+  //   );
+  // }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Treding Products".text.xl2.make(),
+      ],
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items!.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items![index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+  const CatalogItem({super.key, required this.catalog});
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(
+          image: catalog.image,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catalog.desc.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: EdgeInsets.zero,
+              children: [
+                '\$${catalog.price}'.text.bold.xl.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(StadiumBorder()),
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor)),
+                    child: "Buy".text.color(Colors.white).make())
+              ],
+            ).pOnly(right: 8)
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.lightBlue,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0,
@@ -12,7 +12,12 @@ class MyTheme {
           textTheme: Theme.of(context).textTheme,
         ),
       );
+
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  //Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
