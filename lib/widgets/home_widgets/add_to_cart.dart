c621607870d63ec7f704c2455fab6537bee95bd5
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-05-15 06:20:27 +0530

    initilize VxState

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -6,20 +6,15 @@ import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatefulWidget {
   final Item catalog;
-  const AddToCart({
+  AddToCart({
     Key key,
     this.catalog,
   }) : super(key: key);
 
-  @override
-  _AddToCartState createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
@@ -27,8 +22,8 @@ class _AddToCartState extends State<AddToCart> {
           final _catalog = CatalogModel();
 
           _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
+          _cart.add(catalog);
+          // setState(() {});
         }
       },
       style: ButtonStyle(
